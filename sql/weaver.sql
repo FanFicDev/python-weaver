19c22ae40d1a499899d2c23b89f6a7a53853ba1a
iris <iris@serv.pink>
2021-05-15 18:24:59 -0400

    Initial re-import

---
+++ sql/weaver.sql
@@ -0,0 +1,82 @@
+--create extension if not exists pg_trgm;
+
+create domain url as varchar(2048);
+
+create domain oil_timestamp as bigint;
+create function oil_timestamp() returns oil_timestamp language sql stable as $$
+	select (extract(epoch from current_timestamp) * 1000) :: oil_timestamp;
+$$;
+
+create table if not exists encoding (
+	id smallserial primary key,
+	name varchar(256) not null unique
+);
+
+insert into encoding(name)
+select name from (select 'utf8' as name) ei
+where not exists (select id from encoding e where e.name = ei.name);
+
+insert into encoding(name)
+select name from (select 'cp1252' as name) ei
+where not exists (select id from encoding e where e.name = ei.name);
+
+create table if not exists web_source (
+	id smallserial primary key,
+	name varchar(64) not null,
+	source varchar(64),
+	description varchar(256)
+);
+
+create table if not exists web_base (
+	id smallserial primary key,
+	created oil_timestamp,
+	encoding smallint references encoding(id),
+	response bytea
+);
+
+create table if not exists web (
+	id bigserial primary key,
+	created oil_timestamp,
+	url url not null,
+	status smallint not null,
+	sourceId smallint references web_source(id),
+	encoding smallint references encoding(id),
+	response bytea,
+	-- probably null
+	requestHeaders bytea,
+	responseHeaders bytea,
+	wbaseId smallint references web_base(id)
+);
+
+create index if not exists web_idx on web (url, status, created);
+create index if not exists web_url_trgm_idx on web using gin (url gin_trgm_ops);
+
+create or replace view web_size as
+select w.id, w.created,
+	replace(w.url, 'https://www.', '')::url as url,
+	w.status,
+	ws.name as sourceName, ws.description as sourceDescription,
+	e.name as encoding,
+	octet_length(w.response) as responseLength,
+	octet_length(w.requestHeaders) as requestHeadersLength,
+	octet_length(w.responseHeaders) as responseHeadersLength
+from web w
+left join web_source ws on ws.id = w.sourceId
+left join encoding e on e.id = w.encoding
+order by w.created desc;
+
+create table if not exists web_queue (
+	id bigserial primary key,
+	created oil_timestamp,
+	url url not null,
+	status smallint,
+	workerId int4,
+	touched oil_timestamp,
+	stale oil_timestamp not null,
+	musty oil_timestamp not null,
+	priority int4
+);
+
+create index if not exists web_queue_idx on web_queue (url, status);
+create index if not exists web_queue_url_trgm_idx on web_queue using gin (url gin_trgm_ops);
+
--- src/enc.py
+++ src/enc.py
@@ -4,18 +4,18 @@ import oil.util as util
 
 decodeFailureDumpFile = '/tmp/enc_decodeFailure.html'
 
-# this is a set of transforms for turning cp1252 values into their utf8
-# equivalent or an empty bytestring
-cp1252: List[Tuple[bytes, bytes]] = []
-
 # in many cases we have cp1252 embedded into a page with utf8 on the same
-# page; this is used to munge the utf8 into cp1252 so the cp1252 => utf8
+# page; this is used to munge the utf8 into cp1252 so the cp1252 decode
 # process won't choke as hard
 utf8_to_cp1252: List[Tuple[bytes, bytes]] = []
 
+# this is a set of cleanup transforms on (an assumed) cp1252 input to
+# normalize things like smart quotes and delete completely invalid chars
+cp1252_munge: List[Tuple[bytes, bytes]] = []
+
 def setupCP1252() -> None:
-	global cp1252, utf8_to_cp1252
-	if len(cp1252) > 0 and len(utf8_to_cp1252) > 0:
+	global cp1252_munge, utf8_to_cp1252
+	if len(cp1252_munge) > 0 and len(utf8_to_cp1252) > 0:
 		return
 	utf8_to_cp1252 = [
 			(b'\xc2\xa9', b'\xa9'), # ©
@@ -32,80 +32,24 @@ def setupCP1252() -> None:
 			(b'\xe2\x80\x9d', b'\x94'), # ”
 			(b'\xef\xbf\xbd', b'\x81'), # literal question mark block >_>
 		]
-	cp1252 = [
-			# invalid, convert to literal question mark block
-			(b'\x81', b'\xef\xbf\xbd'),
-
-			(b'\x80', b'\xe2\x82\xac'), # €
-			(b'\x82', b'\xe2\x80\x9a'), # ‚
-			(b'\x83', b'\xc6\x92'), # ƒ
-			(b'\x84', b'\xe2\x80\x9e'), # „
-			(b'\x85', b'\xe2\x80\xa6'), # …
-			(b'\x88', b'\xcb\x86'), # ˆ
-			(b'\x8b', b'\xe2\x80\xb9'), # ‹
+	# FIXME these are weird:
+	#   \x98 ˜    \xa6 ¦
+	cp1252_munge = [
+			(b'\x81', b''), # invalid
 			(b'\x91', b"'"), # ‘
 			(b'\x92', b"'"), # ’
 			(b'\x93', b'"'), # “
 			(b'\x94', b'"'), # ”
 			(b'\x96', b'-'), # –
 			(b'\x97', b'-'), # —
-			(b'\x99', b'\xe2\x84\xa2'), # ™
-			(b'\x9c', b'\xc5\x93'), # œ
 			(b'\x9d', b''), # undefined
-			# (b'\x9d', b'\xc5\x93'),
-			(b'\x9e', b'\xc5\xbe'), # ž
 			(b'\xa0', b' '), # nbsp
-			(b'\xa7', b'\xc2\xa7'), # §
-			(b'\xa8', b'\xc2\xa8'), # ¨
-			(b'\xa9', b'\xc2\xa9'), # ©
-			(b'\xac', b'\xc2\xac'), # ¬
 			(b'\xad', b''), # soft hyphen
-			(b'\xb0', b'\xc2\xb0'), # °
-			(b'\xb2', b'\xc2\xb2'), # ²
-			(b'\xb3', b'\xc2\xb3'), # ³
-			(b'\xb4', b'\xc2\xb4'), # ´
-			(b'\xb5', b'\xc2\xb5'), # µ
-			(b'\xb6', b'\xc2\xb6'), # ¶
-			(b'\xb8', b'\xc2\xb8'), # ¸
-			(b'\xb9', b'\xc2\xb9'), # ¹
-			(b'\xba', b'\xc2\xba'), # º
-			(b'\xbc', b'\xc2\xbc'), # ¼
-			(b'\xbd', b'\xc2\xbd'), # ½
-			(b'\xbe', b'\xc2\xbe'), # ¾
-			(b'\xbf', b'\xc2\xbf'), # ¿
-			(b'\xc2', b'\xc3\x82'), # Â
-			(b'\xc3', b'\xc3\x83'), # Ã
-			(b'\xc4', b'\xc3\x84'), # Ä
-			(b'\xdb', b'\xc3\x9b'), # Û
-			(b'\xdf', b'\xc3\x9f'), # ß
-			(b'\xe0', b'\xc3\xa0'), # à
-			(b'\xe1', b'\xc3\xa1'), # á
-			(b'\xe2', b'\xc3\xa2'), # â
-			(b'\xe4', b'\xc3\xa4'), # ä
-			(b'\xe6', b'\xc3\xa6'), # æ
-			(b'\xe7', b'\xc3\xa7'), # ç
-			(b'\xe8', b'\xc3\xa8'), # è
-			(b'\xe9', b'\xc3\xa9'), # é
-			(b'\xea', b'\xc3\xaa'), # ê
-			(b'\xeb', b'\xc3\xab'), # ë
-			(b'\xec', b'\xc3\xac'), # ì
-			(b'\xed', b'\xc3\xad'), # í
-			(b'\xee', b'\xc3\xae'), # î
-			(b'\xef', b'\xc3\xaf'), # ï
-			(b'\xf1', b'\xc3\xb1'), # ñ
-			(b'\xf2', b'\xc3\xb2'), # ò
-			(b'\xf3', b'\xc3\xb3'), # ó
-			(b'\xf4', b'\xc3\xb4'), # ô
-			(b'\xf6', b'\xc3\xb6'), # ö
-			(b'\xf8', b'\xc3\xb8'), # ø
-			(b'\xfa', b'\xc3\xba'), # ú
-			(b'\xfb', b'\xc3\xbb'), # û
-			(b'\xfc', b'\xc3\xbc'), # ü
 		]
 
 # attempt to decode the bytes in data as utf8 and then as munged cp1252
 # returns (encoding, decoded str)
-# returns (None, None) if something goes wrong
+# returns None if something goes wrong
 def decode(data: Optional[bytes], url: str) -> Optional[Tuple[str, str]]:
 	global decodeFailureDumpFile
 	if data is None:
@@ -135,41 +79,19 @@ def decode(data: Optional[bytes], url: str) -> Optional[Tuple[str, str]]:
 	data = data.replace(b'&#8211;\xb9 &#8211; &#8212;\x83',
 			b'#8211;&#8211;&#8211; &#8211; &#8212;&#8')
 
-
-	cm = { c[0]: c[1] for c in cp1252 }
-	minBin = int(cp1252[0][0][0])
-	for c in cp1252:
-		minBin = min(minBin, int(c[0][0]))
-
+	# replace misencoded utf-8 bits (likely from a header or footer) with their
+	# cp1252 counterparts
 	for utoc in utf8_to_cp1252:
 		data = data.replace(utoc[0], utoc[1])
 
-	ndata = bytes(''.encode('utf-8'))
-	idx = -1
-	datalen = len(data)
-	while idx < datalen:
-		idx += 1
-		# if we hit a run of good chars, just copy them wholesale
-		eidx = idx
-		while eidx < datalen and data[eidx] < minBin:
-			eidx += 1
-		if eidx > idx:
-			ndata += data[idx:eidx]
-			idx = eidx - 1
-			continue
-
-		if data[idx:idx+1] not in cm:
-			ndata += data[idx:idx+1]
-			continue
-		ndata += cm[data[idx:idx+1]]
+	# do some cleanup on the remaining cp1252 to normalize smart quotes and
+	# delete a few invalid chars that may have leaked through
+	for ctom in cp1252_munge:
+		data = data.replace(ctom[0], ctom[1])
 
 	try:
-		dec = ndata.decode('utf-8')
+		dec = data.decode('cp1252')
 		return ('cp1252', dec)
-	except UnicodeDecodeError as error:
-		util.logMessage('error decoding {}: {}\n{}'.format(url, error, traceback.format_exc()))
-		util.logMessage('{}'.format((error.args[0], error.args[2], error.args[3], error.args[4])))
-		util.logMessage('{!r}'.format(ndata[max(0, error.args[2] - 20):min(len(ndata), error.args[3] + 20)]))
 	except Exception as e:
 		util.logMessage('error decoding {}: {}\n{}'.format(url, e, traceback.format_exc()))
 
